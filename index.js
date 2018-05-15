40288922ed51264416526d1247d4946507915749
Nick Kreeger <nick.kreeger@gmail.com>
2018-05-15 13:51:54 -0700

    Add histogram plot.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import * as tf from '@tensorflow/tfjs';
 
 import {StrikeZoneData} from './data';
 import {StrikeZoneModel} from './model';
+import {draw} from './plot';
 
 const data = new StrikeZoneData('http://localhost:1234/data.csv', 100);
 const model = new StrikeZoneModel();
@@ -9,12 +10,22 @@ const model = new StrikeZoneModel();
 async function start() {
   await data.load();
 
-  model.train(1, data.batches, (result) => {
-    console.log(`step ${model.steps}] loss: ${
-        result.loss.toFixed(4)} accuracy: ${result.accuracy.toFixed(4)}`);
-  });
+  // model.train(1, data.batches, (result) => {
+  //   console.log(`step ${model.steps}] loss: ${
+  //       result.loss.toFixed(4)} accuracy: ${result.accuracy.toFixed(4)}`);
+  // });
 
   const result = model.predict(data.zone());
+
+  // Draw out 2d histogram of training data:
+  const pxs = [];
+  const pzs = [];
+  data.data.forEach((d) => {
+    pxs.push(d.x[0]);
+    pzs.push(d.x[1]);
+  });
+
+  draw(pxs, pzs);
 }
 
 start();
---
+++ plot.js
@@ -0,0 +1,50 @@
+
+export function draw(px, pz) {
+  var trace1 = {
+    x: px,
+    y: pz,
+    mode: 'markers',
+    name: 'points',
+    marker: {color: 'rgb(102,0,0)', size: 2, opacity: 0.4},
+    type: 'scatter'
+  };
+  var trace2 = {
+    x: px,
+    y: pz,
+    name: 'density',
+    ncontours: 20,
+    colorscale: 'Hot',
+    reversescale: true,
+    showscale: false,
+    type: 'histogram2dcontour'
+  };
+  var trace3 = {
+    x: px,
+    name: 'x density',
+    marker: {color: 'rgb(102,0,0)'},
+    yaxis: 'y2',
+    type: 'histogram'
+  };
+  var trace4 = {
+    y: pz,
+    name: 'y density',
+    marker: {color: 'rgb(102,0,0)'},
+    xaxis: 'x2',
+    type: 'histogram'
+  };
+  var data = [trace1, trace2, trace3, trace4];
+  var layout = {
+    showlegend: false,
+    autosize: false,
+    width: 600,
+    height: 550,
+    margin: {t: 50},
+    hovermode: 'closest',
+    bargap: 0,
+    xaxis: {domain: [0, 0.85], showgrid: false, zeroline: false},
+    yaxis: {domain: [0, 0.85], showgrid: false, zeroline: false},
+    xaxis2: {domain: [0.85, 1], showgrid: false, zeroline: false},
+    yaxis2: {domain: [0.85, 1], showgrid: false, zeroline: false}
+  };
+  Plotly.newPlot('histogram', data, layout);
+}
