9519175862a39a6c5801e86c5a45174cf59e4ccf
Nick Kreeger <nick.kreeger@gmail.com>
2018-05-14 18:53:45 -0700

    Smaller training data.

--- data.js
+++ data.js
@@ -4,20 +4,22 @@ import request from 'request';
 // Training data min/max
 const PX_MIN = -4.45762067272607;
 const PX_MAX = 3.85448981704365;
-const PZ_MIN = -3.76231226128815;
-const PZ_MAX = 11.180078178590072;
-const SZ_TOP_MIN = 1.7929009850154356;
-const SZ_TOP_MAX = 4.237597144526745;
-const SZ_BOT_MIN = 1.0886030433281277;
-const SZ_BOT_MAX = 3.290477690029501;
+const PZ_MIN = -1.61563125700913;
+const PZ_MAX = 6.26978752306894;
+const SZ_TOP_MIN = 2.5935608664315772;
+const SZ_TOP_MAX = 4.168012842802437;
+const SZ_BOT_MIN = 1.101950458012329;
+const SZ_BOT_MAX = 2.0567841383940433;
 
 export async function loadTrainingData(path) {
   request(path, (err, response) => {
     const values = response.body.split(/\r|\n/);
-    // values.forEach((value) => {
-    // });
-    console.log('left off right here', values.length);
-    console.log('test', values[0].split(','));
+    values.forEach((value) => {
+      const values = value.split(',');
+      if (values.length > 0) {
+        console.log('values.length', values.length);
+      }
+    });
   });
 }
 
